da4bfbef54ca13e1df7067f100e6e01c2a70a438
Josh C <josh@joshdotc.dev>
2024-05-15 14:23:13 -0400

    Move SCons options into into custom tool

--- libcGillesPy3D/site_scons/site_tools/toolchains/__init__.py
+++ libcGillesPy3D/site_scons/site_tools/toolchains/__init__.py
@@ -1,5 +1,12 @@
 from . import default, msvc
 
+def options(opts):
+    opts.Add("CC")
+    opts.Add("CFLAGS")
+    opts.Add("CXX")
+    opts.Add("CXXFLAGS")
+    opts.Add("LD")
+
 def exists(env):
     return default.exists(env) or msvc.exists(env)
 
