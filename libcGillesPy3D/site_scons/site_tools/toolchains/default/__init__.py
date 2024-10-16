a853373aeabee9ddc05460740ef69e2a41031b18
Josh C <josh@joshdotc.dev>
2024-10-16 09:38:50 -0400

    Fix issue with variadic C++ standard in SCons tool

--- libcGillesPy3D/site_scons/site_tools/toolchains/default/__init__.py
+++ libcGillesPy3D/site_scons/site_tools/toolchains/default/__init__.py
@@ -4,5 +4,4 @@ def exists(env):
     return True
 
 def generate(env):
-    if "TOOLCHAIN_CXX_STANDARD" in env:
-        env.Append(CXXFLAGS=CLVar("-std=c++${TOOLCHAIN_CXX_STANDARD}"))
+    env.Append(CXXFLAGS=CLVar("-std=c++$TOOLCHAIN_CXX_STANDARD"))
