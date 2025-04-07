cff8f7f8fb0c51cb8e836e9a011f3743f83f3706
MaxwellChalmers <156827512+MaxwellChalmers@users.noreply.github.com>
2025-04-07 13:54:01 -0400

    Update __version__.py

--- model_builder/__version__.py
+++ model_builder/__version__.py
@@ -6,12 +6,12 @@
 # =============================================================================
 
 __version__ = '2.5.2'
-__title__ = 'StochSS'
-__description__ = 'StochSS is an integrated development environment (IDE) \
+__title__ = 'GillesPy3D'
+__description__ = 'GillesPy3D is an integrated development environment (IDE) \
                     for simulation of biochemical networks.'
-__url__ = 'https://github.com/StochSS/model_builder'
+__url__ = 'https://github.com/GillesPy3D/GillesPy3D'
 __download_url__ = ''
-__author__ = 'StochSS Team'
+__author__ = 'GillesPy3D Team'
 __email__ = 'bdrawert@unca.edu'
 __license__ = 'GPL License'
 __copyright__ = 'Copyright (c) 2008-2023'
