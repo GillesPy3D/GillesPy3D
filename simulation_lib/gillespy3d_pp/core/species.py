84ca2726f1033be149a5b4b0e0aa570fd27bb661
thegolriz <anisgolriz714@gmail.com>
2025-09-11 10:38:39 -0400

    Updated species.py

--- simulation_lib/gillespy3d_pp/core/species.py
+++ simulation_lib/gillespy3d_pp/core/species.py
@@ -13,8 +13,8 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from gillespy3d.core.parameter import Parameter
-from gillespy3d.core.error import SpeciesError
+from gillespy3d_pp.core.parameter import Parameter
+from gillespy3d_pp.core.error import SpeciesError
 
 class Species():
     """
